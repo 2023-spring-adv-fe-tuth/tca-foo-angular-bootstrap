26ccbdc7000c7967301d9e7fd4393d08130a4eb8
tsteelematc <tsteele@madisoncollege.edu>
2023-02-22 18:05:21 -0600

    Kind of working...
    But no navigate(-2) analog : - (
    
    Still searching...

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PlayComponent } from './play/play.component';
+import { SetupComponent } from './setup/setup.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: ""
+    , component: HomeComponent
+  }
+  , {
+    path: "setup"
+    , component: SetupComponent
+  }
+  , {
+    path: "play"
+    , component: PlayComponent
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(
+      routes
+      , {
+        useHash: true
+      }
+    )
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-play',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class PlayComponent {
 
+  constructor(
+    private router: Router  
+  ) {};
+
+  gameOver = () => {
+    this.router.navigateByUrl("/");
+  };
 }
