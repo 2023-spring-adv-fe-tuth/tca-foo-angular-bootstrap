d64d8dc10e0e457398edcebb4583d79efac2f152
tsteelematc <tsteele@madisoncollege.edu>
2023-02-23 13:23:48 -0600

    Generated 3 new components...
    ng generate component Home --skip-tests
    
    ng generate component Setup --skip-tests
    
    ng generate component Play --skip-tests

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './home/home.component';
+import { SetupComponent } from './setup/setup.component';
+import { PlayComponent } from './play/play.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    SetupComponent,
+    PlayComponent
   ],
   imports: [
     BrowserModule,
