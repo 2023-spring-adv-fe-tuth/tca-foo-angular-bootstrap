a392629b6d0303b076cb5f09a60885794dc4c60c
tsteelematc <tsteele@madisoncollege.edu>
2023-02-23 13:55:15 -0600

    Working ! ! !
    I'm tired : - )

--- src/app/play/play.component.ts
+++ src/app/play/play.component.ts
@@ -1,3 +1,4 @@
+import { Location } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class PlayComponent {
 
+  constructor(
+    private location: Location
+  ) {};
+
+  endGame = () => {
+    this.location.historyGo(-2);
+  };
 }
--- src/app/setup/setup.component.ts
+++ src/app/setup/setup.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-setup',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class SetupComponent {
 
+  constructor(
+    private router: Router
+  ) {};
+
+  startGame = () => {
+    this.router.navigateByUrl("/play");
+  };
 }
