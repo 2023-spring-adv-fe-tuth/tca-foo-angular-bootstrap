c72c381394bd4775d2b550bb8a3751484b171e56
tsteelematc <tsteele@madisoncollege.edu>
2023-02-28 13:29:27 -0600

    Phew, DI and initial calculateLeaderboard() working...

--- src/app/game.service.ts
+++ src/app/game.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@angular/core';
+import { 
+  GameResult
+  , calculateLeaderboard
+} from './front-end-model';
+
+const hardcodedGameResults: GameResult[] = [
+  {
+      winner: "Tom"
+      , players: ["Tom", "Taylor"]
+  }
+  , {
+      winner: "Taylor"
+      , players: ["Jack", "Taylor"]
+  }
+  , {
+      winner: "Taylor"
+      , players: ["Tom", "Taylor", "Jack"]
+  }
+  , {
+      winner: "X"
+      , players: ["X", "Joe"]
+  }
+  , {
+      winner: "X"
+      , players: ["X", "Joe"]
+  }
+  , {
+      winner: "Joe"
+      , players: ["X", "Joe"]
+  }
+  , {
+      winner: "Jack"
+      , players: ["X", "Joe", "Jack"]
+  }
+];
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +41,10 @@ import { Injectable } from '@angular/core';
 export class GameService {
 
   constructor() { }
+
+  gameResults: GameResult[] = hardcodedGameResults;
+
+  calculateLeadboard = () => {
+    return calculateLeaderboard(this.gameResults);
+  };
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { LeaderboardPlayer } from '../front-end-model';
+import { GameService } from '../game.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  constructor(
+    private gameSvc: GameService
+  ) {}
+
+  leaderboardData: LeaderboardPlayer[] = [];
+
+  ngOnInit(): void {
+    this.leaderboardData = this.gameSvc.calculateLeadboard();
+    console.log(this.leaderboardData);
+  }
 }
