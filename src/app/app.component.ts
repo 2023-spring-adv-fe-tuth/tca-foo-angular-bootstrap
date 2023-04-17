2ec5e69dd349647a2d175d62de5704c9d7cff68a
tsteelematc <tsteele@madisoncollege.edu>
2023-04-17 15:22:16 -0500

    Boom ! ! !

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as localforage from 'localforage';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'tca-foo-angular-bootstrap';
+
+  emailKey = "";
+
+  async ngOnInit() {
+    console.log("ngOnInit");
+    this.emailKey = await localforage.getItem("emailKey") ?? "";
+  }
+
+  saveEmailKey = async () => {
+    try {
+      await localforage.setItem("emailKey", this.emailKey)
+    }
+    catch (err){
+      console.error(err);
+    }
+  };
 }
