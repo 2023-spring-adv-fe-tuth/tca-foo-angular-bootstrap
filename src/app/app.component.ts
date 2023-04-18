046d5721f40622d42b2825786889cf1037e9fbcb
tsteelematc <tsteele@madisoncollege.edu>
2023-04-18 13:05:11 -0500

    Finishe misc/email-for-cloud-storage...
    It included the UI and the localforage nonsense ! ! !

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as localforage from 'localforage';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'tca-foo-angular-bootstrap';
+
+
+  async ngOnInit() {
+
+    try {
+      this.emailAddress = await localforage.getItem("emailAddress") ?? "";
+    }
+    catch (err) {
+      console.error(err);
+    }
+  }
+
+  emailAddress = "";
+
+  saveEmailAddress = async () => {
+    try {
+      await localforage.setItem(
+        "emailAddress"
+        , this.emailAddress
+      );
+    }
+    catch (err) {
+      console.error(err);
+    }
+  };
 }
